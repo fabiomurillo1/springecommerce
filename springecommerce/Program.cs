3c49eabd1a587c6ce637f89cf755eb38d7fa6d98
Fabio Murillo <fabiomurillo2506@gmail.com>
2025-03-31 00:01:07 -0400

    added models and modified program.cs

--- springecommerce/Program.cs
+++ springecommerce/Program.cs
@@ -4,8 +4,47 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            
+            Console.WriteLine("Welcome to Amazon!");
+            Console.WriteLine("C. Create Item");
+            Console.WriteLine("R. Read inventory items");
+            Console.WriteLine("U. Update Inventory Item");
+            Console.WriteLine("D. Delete Inventory Item");
+            Console.WriteLine("Q. Quit");
+
+            List<string?> list = new List<string?>();
+
+            char choice;
+            do
+            {
+                string? input = Console.ReadLine().ToUpper();
+                choice = input[0];
+                switch (choice)
+                {
+
+                    case 'C':
+                        AddProduct(list);
+                        break;
+                    case 'R':
+
+                    case 'U':
+                    case 'D':
+                    case 'Q':
+                        break;
+                    default:
+                        Console.WriteLine("Error : Unknown Command");
+                        break;
+
+                }
+            } while (choice != 'Q');
+        }
+
+
+
+        static void AddProduct(List<string> list)
+        {
+            var newProduct = Console.ReadLine() ?? "N/A";
+            list.Add(newProduct);
         }
     }
+
 }
