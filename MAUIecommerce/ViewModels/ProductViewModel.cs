ceff5110a43b2772e9981045d82197668add447a
Fabio Murillo <fabiomurillo2506@gmail.com>
2025-04-10 02:15:09 -0400

    Added ok button to add items

--- MAUIecommerce/ViewModels/ProductViewModel.cs
+++ MAUIecommerce/ViewModels/ProductViewModel.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace MAUIecommerce.ViewModels
 {
-    internal class ProductViewModel
+    public class ProductViewModel
     {
     }
 }
--- MAUIecommerce/Views/ProductDetail.xaml.cs
+++ MAUIecommerce/Views/ProductDetail.xaml.cs
@@ -1,3 +1,6 @@
+using MAUIecommerce.ViewModels;
+using springecommerce.models;
+
 namespace MAUIecommerce.Views;
 
 public partial class ProductDetail : ContentPage
@@ -5,6 +8,7 @@ public partial class ProductDetail : ContentPage
 	public ProductDetail()
 	{
 		InitializeComponent();
+        BindingContext = new ProductViewModel();
 	}
 
     private void GoBackClicked(object sender, EventArgs e)
